40890f7937d8d800cefb1450e193fdb55d2cd727
Moscow12 <meshackstev@gmail.com>
2018-09-20 10:06:08 -0700

    div table added

--- version1/application/views/use/view_time_tservice.php
+++ version1/application/views/use/view_time_tservice.php
@@ -1,11 +1,33 @@
-<div class="row">
-    <div class="col-md-3"></div>
-    <div class=" col-md-6">
-        <table>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-        </table>
+
+
+
+    <div class="row"><div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card card-header"><?php #echo $title; ?></div>
+                <div class="card card-body">
+                    <table class="table-striped table-bordered ">
+                    <thead>
+                        <th>#</th>
+                        <th>Service name</th>
+                        <th>Day</th> 
+                        <th>Start time</th>
+                        <th> End time</th>
+                    </thead>
+                    <tbody>
+                    <?php $namb=1; foreach($times as $tim){?>
+                        <tr>
+                            <th><?php echo $namb++; ?></th>
+                            <td><?php echo $tim['service_name'];?> </td>
+                            <td><?php echo $tim['day'];?> </td>
+                            <td><?php echo $tim['start_at'];?> </td>
+                            <td><?php echo $tim['end_at'];?> </td>                         
+                        </tr>
+                        <?php }?>
+                    </tbody>
+                    </table>
+                </div>
+            </div><a href="article" class="btn btn-info">Add Article</a>
+            <div class="col-md-1"></div>
+        </div>
     </div>
-</div>
