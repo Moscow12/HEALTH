be97e62bc58bef9edc1f76b0d402ca480f06e40a
Moscow12 <meshackstev@gmail.com>
2018-09-20 12:25:54 -0700

    adding function for deleting and editing

--- version1/application/controllers/Register.php
+++ version1/application/controllers/Register.php
@@ -123,6 +123,93 @@
 			}
 		}
 
+		//function to view services
+		public function view_service(){
+			
+			$data['title'] = "This is your registered service u offer";
+			$data['Services'] = $this->Register_model->get_services();
+
+			$this->load->view('use/header');
+			$this->load->view('use/vservices', $data);
+			$this->load->view('use/footer'); 
+		}
+
+		public function edit_service(){
+			$id = $this->uri->segment(3);
+			$data['title'] = "Edit the service";			
+			#$data['edit'] = $this->Register_model->get_service();
+			$data['edit'] = $this->Register_model->get_service_id($id);
+
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('service_name', 'Service Name', 'required');
+			$this->form_validation->set_rules('description', 'Service Description', 'required');
+
+			if($this->form_validation->run()===FALSE){
+
+			$this->load->view('use/header');
+			$this->load->view('use/editservices', $data);
+			$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'service_name'=>$this->input->post('service_name'),
+					'description' => $this->input->post('description'),
+            		'user_id' =>$this->session->userdata('user_id')
+				);
+
+				if($this->session->userdata('user_id') === NULL){
+				redirect('Users/login');
+				}else{
+					$this->Register_model->update_service($data, $id);
+					$this->session->set_flashdata('doctor_service', 'Services you offer has been set');
+
+					redirect('Register/view_service');
+				}
+			}
+
+		}
+
+		//Update service
+		public function dr_update_service(){
+			$data['title'] = "Update the service";
+			$id= $this->input->post('id');
+			$data['event'] = $this->Register_model->get_service_id($id);
+
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('service_name', 'Service Name', 'required');
+			$this->form_validation->set_rules('description', 'Service Description', 'required');
+
+			if($this->form_validation->run()===FALSE){
+
+			$this->load->view('use/header');
+			$this->load->view('use/editservices', $data);
+			$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'service_name'=>$this->input->post('service_name'),
+					'description' => $this->input->post('description'),
+            		'user_id' =>$this->session->userdata('user_id')
+				);
+
+				if($this->session->userdata('user_id') === NULL){
+				redirect('Users/login');
+				}else{
+					$this->Register_model->dr_service_update($data, $id);
+					$this->session->set_flashdata('updated_service', 'Service has been update successfully');
+
+					redirect('Register/view_service');
+				}
+			}
+
+		}
+
+		public function delete($id){
+			$id = $this->uri->segment(3);
+			$this->Register_model->delete_service_id($id);
+
+			$this->session->set_flashdata('service_delete', 'The service has been deleted');
+			redirect('Register/view_service');
+		}
+
 		public function service_time(){
 			$data['title'] = "Fill in time for the service you offer";
 
@@ -142,7 +229,8 @@
 			}else{
 				$data = array(
 					'day' => $this->input->post('day'),
-					'time' => $this->input->post('time'),
+					'start_at' => $this->input->post('start_at'),
+					'end_at' => $this->input->post('end_at'),
 					'service_id' =>$this->input->post('service_id')
 				);
 				if($this->session->userdata('user_id')===NULL ){
@@ -155,59 +243,17 @@
 			}
 		}
 
-		public function view_service(){
-			
-			$data['title'] = "This is your registered service u offer";
-			$data['Services'] = $this->Register_model->get_services();
+		//function to view time for the service 
+		public function view_time(){
+			$data['title'] = "This is the arrangement of time with their services";
+			$data['times'] = $this->Register_model->get_time();
 
 			$this->load->view('use/header');
-			$this->load->view('use/vservices', $data);
-			$this->load->view('use/footer'); 
-		}
-
-		public function edit_service($id){
-			$id = $this->uri->segment(3);
-			$data['title'] = "Edit the service";			
-			$data['edit'] = $this->Register_model->get_service();
-
-			$this->load->library('form_validation');
-			$this->form_validation->set_rules('service_name', 'Service Name', 'required');
-			$this->form_validation->set_rules('description', 'Service Description', 'required');
-
-			if($this->form_validation->run()===FALSE){
-
-			$this->load->view('use/header');
-			$this->load->view('use/editservices', $data);
+			$this->load->view('use/view_time_tservice', $data);
 			$this->load->view('use/footer');
-			}else{
-				$data = array(
-					'service_name'=>$this->input->post('service_name'),
-					'description' => $this->input->post('description'),
-            		'user_id' =>$this->session->userdata('user_id')
-				);
-
-				if($this->session->userdata('user_id') === NULL){
-				redirect('register/login');
-				}else{
-					$this->Register_model->update_service($data, $id);
-					$this->session->set_flashdata('doctor_service', 'Services you offer has been set');
-
-					redirect('Register/view_service');
-				}
-			}
 
 		}
 
-		public function delete($id){
-
-
-			$id = $this->uri->segment(3);
-			$this->Register_model->delete_service_id($id);
-
-
-			$this->session->set_flashdata('service_delete', 'The service has been deleted');
-			redirect('Register/view_service');
-		}
 		//Function for posting events doctor
 		public function event(){
 			$data['title'] = "Post your Event vacation";
@@ -229,8 +275,57 @@
 					'description' => $this->input->post('description'),
 					'location' => $this->input->post('location'),
 					'photo' => $this->input->post('photo'),
-            		'user_id' =>$this->session->user_id,
-					'date' => $this->input->post('date')
+					'user_id' =>$this->session->userdata('user_id'),
+					'date'=>mdate('%Y-%m-%d %H:%i:%s', now())
+				);
+				
+					$this->Register_model->dr_event($data);
+					$this->session->set_flashdata('doctor_events', 'You have shared your event');
+					redirect('Register/vevent');
+				}
+
+		}
+
+		//function to view the event doctor posted 
+		public function vevent(){
+			$data['title'] = "This is Your Event you posted";
+			$data['events'] =$this->Register_model->get_events();
+
+			$this->load->view('use/header');
+			$this->load->view('use/view_dr_event', $data);
+			$this->load->view('use/footer');
+		}
+
+		//fucntion to edit doctor event posted
+		public function edit_event(){
+
+			$id = $this->uri->segment(3);
+			$data['title'] = "Update the article";
+			$data['event'] = $this->Register_model->get_event_id($id);
+			$data['id'] = $this->uri->segment(3); 
+			#$data['id']
+
+			
+
+			$this->load->library('form_validation');
+
+			$this->form_validation->set_rules('title', 'Title', 'required');
+			$this->form_validation->set_rules('description', 'Description', 'required');
+			$this->form_validation->set_rules('location', 'Location', 'required');
+			$this->form_validation->set_rules('photo', 'Photo', 'required');
+
+			if($this->form_validation->run()===FALSE ){
+				$this->load->view('use/header');
+				$this->load->view('use/dr_edit_event', $data);
+				$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'title' => $this->input->post('title'),
+					'description' => $this->input->post('description'),
+					'location' => $this->input->post('location'),
+					'photo' => $this->input->post('photo'),
+					'user_id' =>$this->session->userdata('user_id'),
+					'date'=>mdate('%Y-%m-%d %H:%i:%s', now())
 				);
 				
 					$this->Register_model->dr_event($data);
@@ -238,6 +333,49 @@
 					redirect('Register/event');
 				}
 
+		}
+
+		//function to update event posted by doctor
+		public function update_event(){
+			$data['title'] = "Update the event";
+			$id= $this->input->post('id');
+			$data['event'] = $this->Register_model->get_event_id($id);
+
+			$this->load->library('form_validation');
+
+			$this->form_validation->set_rules('title', 'Title', 'required');
+			$this->form_validation->set_rules('description', 'Description', 'required');
+			$this->form_validation->set_rules('location', 'Location', 'required');
+			$this->form_validation->set_rules('photo', 'Photo', 'required');
+
+			if($this->form_validation->run()===FALSE ){
+				$this->load->view('use/header');
+				$this->load->view('use/dr_edit_event', $data);
+				$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'title' => $this->input->post('title'),
+					'description' => $this->input->post('description'),
+					'location' => $this->input->post('location'),
+					'photo' => $this->input->post('photo'),
+					'user_id' =>$this->session->userdata('user_id'),
+					'date'=>mdate('%Y-%m-%d %H:%i:%s', now())
+				);
+
+			
+					$this->Register_model->update_event($data, $id);
+					$this->session->set_flashdata('doctor_events', 'You have shared your event');
+					redirect('Register/vevent');
+				}
+		}
+
+		//function to delete the event
+		public function delete_evt(){
+			$id = $this->uri->segment(3);
+			$this->Register_model->delete_event_id($id);
+
+			$this->session->set_flashdata('event_deleted', 'The service has been deleted');
+			redirect('Register/vevent');
 		}
 
 		public function article(){
@@ -256,17 +394,99 @@
 				$data = array(
 					'title' => $this->input->post('title'),
 					'description' => $this->input->post('description'),
-					'post_date' => $this->input->post('post_date'),
+					'date' => $this->input->post('date'),
             		'user_id' =>$this->session->userdata('user_id'),
 				);
 				if($this->session->userdata('user_id')===NULL){
 					redirect('Register/login');
 				}else{
 					$this->Register_model->dr_article($data);
-					$this->session->set_flashdata('doctor_article', 'You have shared your Article');
-					redirect('Register/article');
+					$this->session->set_flashdata('article_added', 'You have shared your Article');
+					redirect('Register/varticle');
 				}
 			}
+		}
+
+		public function varticle(){
+			$data['title'] = "This is your article you posted to the website";
+			$data['varticle'] = $this->Register_model->get_article();
+
+			$this->load->view('use/header');
+			$this->load->view('use/view_dr_article', $data);
+			$this->load->view('use/footer');
+
+		}
+
+		public function edit_article(){
+			$id = $this->uri->segment(3);
+			$data['title'] = "Update the article";
+			$data['articles'] = $this->Register_model->get_article_id($id);
+
+			$this->load->library('form_validation');
+
+			$this->form_validation->set_rules('title', 'Articles title', 'required');
+			$this->form_validation->set_rules('description', 'Articles Description', 'required');
+
+			if($this->form_validation->run()===FALSE){
+				$this->load->view('use/header');
+				$this->load->view('use/edit_dr_article', $data);
+				$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'title' => $this->input->post('title'),
+					'description' => $this->input->post('description'),
+					'date' => $this->input->post('date'),
+            		'user_id' =>$this->session->userdata('user_id'),
+				);
+				if($this->session->userdata('user_id')===NULL){
+					redirect('Register/login');
+				}else{
+					$this->Register_model->dr_article_update($data, $id);
+					$this->session->set_flashdata('doctor_article', 'You have shared your Article');
+					redirect('Register/varticle');
+				}
+			}
+		}
+
+		//function to update article
+		public function update_article(){
+			$data['title'] = "Update the article";
+			$id= $this->input->post('id');
+			$data['event'] = $this->Register_model->get_article_id($id);
+
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('title', 'Articles title', 'required');
+			$this->form_validation->set_rules('description', 'Articles Description', 'required');
+
+			if($this->form_validation->run()===FALSE){
+				$this->load->view('use/header');
+				$this->load->view('use/edit_dr_article', $data);
+				$this->load->view('use/footer');
+			}else{
+				$data = array(
+					'title' => $this->input->post('title'),
+					'description' => $this->input->post('description'),
+					'date' => mdate('%Y-%m-%d %H:%i:%s', now()),
+            		'user_id' =>$this->session->userdata('user_id'),
+				);
+				if($this->session->userdata('user_id')===NULL){
+					redirect('Register/login');
+				}else{
+					$this->Register_model->dr_article_update($data, $id);
+					$this->session->set_flashdata('doctor_article', 'You have shared your Article');
+					redirect('Register/varticle');
+				}
+			}
+
+		}
+
+		public function delete_art(){
+
+			$id = $this->uri->segment(3);
+			$this->Register_model->delete_article_id($id);
+
+			$this->session->set_flashdata('article_deleted', 'The service has been deleted');
+			redirect('Register/varticle');
 		}
 
 	}
