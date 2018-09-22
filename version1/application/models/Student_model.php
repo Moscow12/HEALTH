b6cf9351a4bd1d04d33933d26eb0ba8e682479e3
Moscow12 <meshackstev@gmail.com>
2018-09-21 23:09:47 -0700

    created new model for student

---
+++ version1/application/models/Student_model.php
@@ -0,0 +1,37 @@
+<?php 
+    class Student_model extends CI_model{
+    function __Construct(){
+        $this->load->database();
+    }
+
+    public function student_profile($data){
+        return $this->db->insert('student', $data);
+    }
+
+    public function get_profile(){
+        $user = $this->session->userdata('user_id');		
+        $this->db->select('s.name,  email, phone_no, current_location, dob, sex, birth_place, job_status, yeah_of_study, school, dept_name' );
+        $this->db->from( 'sprofile_view s');
+        $this->db->join('department d', 'd.id = s.dept_id', 'inner');
+        $this->db->where('s.id', $user);
+        
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function student_update_profile($data, $id){
+        $this->db->where('id', $id);
+        $this->db->update('student', $data);
+    }
+
+    public function get_article(){
+        $user =$this->session->userdata('user_id');
+        $this->db->select('title, description');
+        $this->db->from('articles');
+        $this->db->where('id', $user);
+
+        $query = $this->db->get();
+        return $query->result_array();
+
+    }
+ }
