5b3219b1510b4e1eb5452098de06f67491f15c89
Moscow12 <meshackstev@gmail.com>
2018-10-04 11:03:56 -0700

    function to add job in a model

--- version1/application/models/Hospital_model.php
+++ version1/application/models/Hospital_model.php
@@ -1,4 +1,21 @@
 <?php 
     class Hospital_model extends CI_Model{
 
+        function __Constract(){
+            $this->load->database();
+        }
+
+        public function post_job($data){
+            $this->db->insert('job', $data);
+        }
+
+        public function get_job(){
+            $user = $this->session->userdata('user_id');
+            $this->db->select('*');
+            $this->db->from('job');
+            $this->db->where('id', $user);
+
+            $query = $this->db->get();
+            return $query->result_array();
+        }
     }
