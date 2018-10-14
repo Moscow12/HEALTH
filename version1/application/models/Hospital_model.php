b8934f6bcc25320bd982a809fd050eae8ec8618a
Moscow12 <meshackstev@gmail.com>
2018-10-13 18:21:43 -0700

    adding more function on the model

--- version1/application/models/Hospital_model.php
+++ version1/application/models/Hospital_model.php
@@ -18,4 +18,33 @@
             $query = $this->db->get();
             return $query->result_array();
         } 
+
+        public function get_description($id){
+            $this->db->where('id', $id);
+			$query = $this->db->get('job');
+			return $query->result_array();
+        }
+
+        public function get_details(){
+            $user = $this->session->userdata('user_id');
+            $this->db->select('*');
+            $this->db->from('users');
+            $this->db->where('user_id', $user);
+        }
+        
+        public function set_profile($data){
+
+			return $this->db->insert('hospital',$data);
+        }
+        
+        public function get_profile(){
+
+            $user = $this->session->userdata('user_id');
+            $this->db->select('name, email, h_phone, country, region, district, ward, street, address');
+            $this->db->from('hosp_profile_view');
+            $this->db->where('id', $user);
+
+            $query = $this->db->get();
+			return $query->result_array();
+        }
     }
