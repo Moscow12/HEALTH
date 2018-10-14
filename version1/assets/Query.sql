e344429e4b661bd91aad903785e6212e51efec2f
Moscow12 <meshackstev@gmail.com>
2018-10-13 18:26:43 -0700

    sql view

--- version1/assets/Query.sql
+++ version1/assets/Query.sql
@@ -9,3 +9,6 @@ from service_time inner JOIN service WHERE service.id = service_time.service_id;
 CREATE VIEW sprofile_view as SELECT users.id, users.name, users.email, student.sex, student.yeah_of_study, student.dob, 
 student.birth_place, student.current_location, student.phone_no, student.school, student.job_status, student.dept_id FROM users 
 inner join student WHERE users.id=student.user_id;
+
+CREATE VIEW hosp_profile_view as SELECT users.id, users.name, users.email, hospital.h_phone, hospital.country, hospital.region, hospital.district,
+hospital.ward, hospital.street, hospital.address FROM users inner join hospital WHERE users.id =  hospital.user_id;
