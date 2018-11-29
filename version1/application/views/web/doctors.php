e1f20dd223377bdd9cd1bc76d7de96992b5f1a2a
Moscow12 <meshackstev@gmail.com>
2018-11-29 05:45:09 -0800

    viewof doctors in a web

--- version1/application/views/web/doctors.php
+++ version1/application/views/web/doctors.php
@@ -1,24 +1,32 @@
-<section id="doctors">
-    <div class="container">
-            <div>
-                <h3><?php echo $title;?></h3><br>
-                <p>Get assisted by our Authorized and professional Doctors </p><br>
-                <small>View their profile</small>
-
+   <!-- Section: team -->
+   <section id="doctor" class="home-section bg-gray paddingbot-20">
+      <div class="container marginbot-10">
+        <div class="row">
+          <div class="col-lg-8 col-lg-offset-2">
+            <div class="wow fadeInDown" data-wow-delay="0.1s">
+              <div class="section-heading text-center">
+                <h2 class="h-bold">Doctors</h2>
+                <p>Meet our proffetional doctors here and get in touch with them.</p>
+              </div>
             </div>
+            <div class="divider-short"></div>
+          </div>
+        </div>
+      </div>
 
+      <div class="container"> 
             <div class="row">
                 <div class="col-md-12">
                     
                         <?php foreach($doctor as $doctors){?>
-                            <div class="col-md-4">
+                            <div class="col-md-3">
                                 <div class="thumbnail">
                                     <a href="<?=base_url();?>assets/img/dummy/img-1.png"><img src="<?=base_url();?>assets/img/dummy/img-1.png" alt=""></a>
                                     <div class="caption">
-                                        Name:<?php echo $doctors['name'];?><br>
-                                        Professional:<?php echo $doctors['professional'];?><br>
-                                        Email:<?php echo $doctors['email'];?><br>
-                                        Phone #: <?php echo $doctors['phone'];?><br><a href="dr_profile/<?php echo $doctors['id'];?>"class="btn btn-info">View profile</a>
+                                       <strong>Name:</strong> <?php echo $doctors['name'];?><br>
+                                        <strong>Professional:</strong> <?php echo $doctors['professional'];?><br>
+                                        <!-- Email:<?php echo $doctors['email'];?><br> -->
+                                       <!-- <strong>Phone #:</strong>  <?php echo $doctors['phone'];?><br>--><a href="<?=base_url();?>index.php/Tanza/dr_profile/<?php echo $doctors['id'];?>"class="btn btn-info">View profile</a> 
                                     </div>
                                     
                                 </div>
@@ -26,7 +34,12 @@
                                 
                             </div>
                         <?php }?>
-                    <nav>
+                   
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 offset-md-4">
+                     <nav>
                             <ul class="pagination">
                                 <li class="page-item disabled">
                                     <span class="page-link">Previous</span>
@@ -37,8 +50,9 @@
                                 <li class="page-item"><a class="page-link">Next</a></li>
                             </ul>
                     </nav>
-                </div>
+                </div>                
             </div>
-    </div>
-   
-</section>
+          </div>
+        </div>
+      </div>
+    </section>
